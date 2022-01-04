c9b3ce761e14a9e53266303c0590cc014cdddb95
Thiago Marciel <thiagomarciel.dev@gmail.com>
2022-01-04 07:39:27 -0300

    usign normalize and assets/scss/base.scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    'normalize.css/normalize.css',
+    '@/assets/scss/base.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
