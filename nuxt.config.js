c977f6a9aa48c358af97af24083b5d74e1fa786d
Thiago Marciel <thiagomarciel.dev@gmail.com>
2022-01-04 07:41:26 -0300

    updated head tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,14 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ibook',
+    title: 'iBook',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'pt-BR'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: 'Uma nova maneira de ler livros online!' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
