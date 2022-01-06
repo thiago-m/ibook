a1e6a190fb7a30d74b8a96300917f6eba4cc45d5
Thiago Marciel <thiagomarciel.dev@gmail.com>
2022-01-06 07:26:31 -0300

    added ibook layout

---
+++ layouts/ibook.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="iBook-layout">
+        <Header />
+        <Container> <Nuxt /> </Container>
+        <Footer />
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        
+    })
+</script>
+
+<style lang="scss" scoped>
+.iBook-layout {
+    margin-top: 4.313rem;
+    display: grid;
+    grid-gap: 6.75rem;
+    .container {
+        padding-bottom: 6.75rem;
+    }
+}
+</style>
