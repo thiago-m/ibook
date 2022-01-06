4e8737ca7dff12b73908bd8e3d87658ed24455ae
Thiago Marciel <thiagomarciel.dev@gmail.com>
2022-01-06 07:51:21 -0300

    added page home

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,5 @@
 <template>
-  <div class="wrapper">
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-  </div>
+  <HomeTemplate />
 </template>
 
 <script lang="ts">
@@ -13,22 +9,3 @@ export default Vue.extend({
   layout: 'ibook'
 })
 </script>
-
-<style lang="scss" scoped>
-.wrapper {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-template-rows: 200px;
-  grid-auto-flow: column;
-  grid-gap: 0.5rem;
-  @include screen('small') {
-    grid-template-columns: 1fr;
-  }
-  @include screen('medium') {
-    grid-template-columns: 1fr 1fr;
-  }
-  .box {
-    background-color: color('dark', 'darkest');
-  }
-}
-</style>
