d4f429db70f26e4fc1a6b6f503418d996469edac
Thiago Marciel <thiagomarciel.dev@gmail.com>
2022-01-06 07:41:38 -0300

    added AboutTemplate template

---
+++ components/templates/AboutTemplate.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="about-template">
+        <PageTitle text="O que é o" strong="iBook" />
+        <Presentation />
+    </div>
+</template>
+
+
+<style scoped>
+.about-template {
+    display: grid;
+    grid-gap: 4rem;
+}
+</style>
