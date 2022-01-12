ade2f52fd4811e2490eb6dd7c4a9efaa1754610e
Thiago Marciel <thiagomarciel.dev@gmail.com>
2022-01-12 07:53:29 -0300

    created list of books

---
+++ components/molecules/BookCard.vue
@@ -0,0 +1,55 @@
+<template>
+    <NuxtLink class="book-card" :to="`/book/${book.id}`">
+        <img :src="book.cover" :alt="`Capa do livro ${book.title}`" >
+        <div class="text">
+            <h4>{{ book.title }}</h4>
+            <p>{{ book.author }}</p>
+        </div>
+    </NuxtLink>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    props: {
+        book: {
+            type: Object,
+            required: true
+        }
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+.book-card {
+    color: inherit;
+    display: grid;
+    grid-gap: 1.5rem;
+    img {
+        display: grid;
+        width: 100%;
+        max-height: 160px;
+        box-shadow: 0px 4px 11px 4px rgba(0, 0, 0, 0.13);
+        transition: all 300ms ease;
+        @include screen('small') {
+            max-height: 300px;
+        }
+        @include screen('medium') {
+            max-height: 300px;
+        }
+        &:hover {
+            transform: scale(1.1);
+            box-shadow: 0px 4px 17px 4px rgba(0, 0, 0, 0.11);
+        }
+    }
+    h4 {
+        font-size: 0.875rem;
+        font-weight: 400;
+    }
+    p {
+        font-size: 13px;
+        color: color('dark');
+    }
+}
+</style>
--- components/organisms/BookList.vue
+++ components/organisms/BookList.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="book-list">
-        <h2>Listagem de livros</h2>
+        <BookCard v-for="book in $books" :key="book.id" :book="book" />
     </div>
 </template>
 
@@ -8,10 +8,25 @@
     import Vue from 'vue'
 
     export default Vue.extend({
-        
+        computed: {
+            $books() {
+              return this.$store.getters['books/$all']
+            }
+        }
     })
 </script>
 
 <style lang="scss" scoped>
-
+.book-list {
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: auto;
+    grid-gap: 2rem;
+    @include screen('small') {
+        grid-template-columns: repeat(2, 1fr);
+    }
+    @include screen('medium') {
+        grid-template-columns: repeat(3, 1fr);
+    }
+}
 </style>
